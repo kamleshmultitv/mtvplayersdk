fff174e5c3cb56fae67c820d9d138cf05fdd562c
Kamlesh Yadav <kamlesh.y@multitvsolution.com>
2026-01-06 12:51:28 +0530

    ima

--- app/src/main/java/com/app/sample/utils/ApiEncryptionHelper.kt
+++ app/src/main/java/com/app/sample/utils/ApiEncryptionHelper.kt
@@ -4,17 +4,6 @@ import android.util.Base64
 import java.nio.charset.StandardCharsets
 
 class ApiEncryptionHelper {
-    fun decryptionResponse(encryptionString: String): String {
-        val mcipher = MultitvCipher()
-        var str: String? = null
-        try {
-            str = String(mcipher.decryptmyapi(encryptionString))
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return str.orEmpty()
-    }
-
     companion object {
         fun convertStringToBase64(text: String): String {
             val data = text.toByteArray(StandardCharsets.UTF_8)
