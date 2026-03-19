eb9a5b1d2944a446f3a9c9b07f6a6f3f6a630d20
Kamlesh <kamlesh@Kamleshs-MacBook-Air.local>
2026-03-19 11:54:54 +0530

    code merge

--- videosdk/build.gradle.kts
+++ videosdk/build.gradle.kts
@@ -68,7 +68,7 @@ afterEvaluate {
 
                 groupId = "com.github.kamleshmultitv"
                 artifactId = "videosdk"
-                version = "mobile-2.0.0"
+                version = "mobile-2.0.3"
             }
         }
     }
