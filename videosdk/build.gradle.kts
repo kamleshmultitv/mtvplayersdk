ee15b811fe7c8807f288b562cab79f5de1146f67
Kamlesh Yadav <kamlesh.y@multitvsolution.com>
2026-08-06 12:10:53 +0530

    code merge

--- videosdk/build.gradle.kts
+++ videosdk/build.gradle.kts
@@ -68,7 +68,7 @@ afterEvaluate {
 
                 groupId = "com.github.kamleshmultitv"
                 artifactId = "videosdk"
-                version = "mobile-2.0.14"
+                version = "mobile-2.0.15"
             }
         }
     }
